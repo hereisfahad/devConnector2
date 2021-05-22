8c0a5369dfeb9ad9e5bd2d656aebc29774f02194
Fahad Jabbar <fahadjabbar31@gmail.com>
2021-05-23 02:38:41 +0500

    fix cors error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import postsRoutes from './routes/api/posts.js'
 
 dotenv.config()
 const app = express()
-app.use(cors(/*{ origin: 'http://localhost:3000', credentials: true } */))
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
